b403b748c0345e1709695ef84cba9ec3c2f4024a
Scott Mielcarski <scott_mielcarski@me.com>
2015-01-28 23:40:02 -0800

    Convert tab indents to spaces.

--- rgb_led.py
+++ rgb_led.py
@@ -1,4 +1,4 @@
-#	RGB LED
+#   RGB LED
 #
 # This script allows the user to change the color of a multi-color LED
 # connected to the GPIO pins of the Raspberry Pi. It assumes that the pins
@@ -7,9 +7,9 @@
 # multi-colored LED wired in series with three resistors: 330 ohms (red), 180
 # ohms (green), and 120 ohms (blue). The cathode of the LED should go to 3.3V.
 #
-# Creator:	Scott Mielcarski
-# Created:	January 26, 2015
-# Modified:	January 28, 2015
+# Creator:  Scott Mielcarski
+# Created:  January 26, 2015
+# Modified: January 28, 2015
 
 
 import RPi.GPIO as GPIO
@@ -17,55 +17,55 @@ import re
 
 
 # list of GPIO pins that should be used
-#	pins[0] -> red
-#	pins[1] -> green
-#	pins[2] -> blue
+#   pins[0] -> red
+#   pins[1] -> green
+#   pins[2] -> blue
 pins = [11, 13, 15]
 
 # mapping of colors to their appropriate RGB value
 colors = {
-	'off':		'000',
-	'blue':		'001',
-	'green':	'010',
-	'teal':		'011',
-	'red':		'100',
-	'purple':	'101',
-	'yellow':	'110',
-	'white':	'111'
+    'off':      '000',
+    'blue':     '001',
+    'green':    '010',
+    'teal':     '011',
+    'red':      '100',
+    'purple':   '101',
+    'yellow':   '110',
+    'white':    '111'
 }
 
 
 
-#	Run
+#   Run
 # This is the main function of the script. It creates a loop which waits for a
 # users input. Whent the input is recieved, the multi-color LED is set
 # accordingly.
 
 def run():
-	# setup the GPIO pins
-	GPIO.setmode(GPIO.BOARD)
-	for pin in pins:
-		GPIO.setup(pin, GPIO.OUT)
-		GPIO.output(pin, 1)
+    # setup the GPIO pins
+    GPIO.setmode(GPIO.BOARD)
+    for pin in pins:
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, 1)
 
-	# loop until the user presses ^C
-	try:
-		while(True):
-			req = raw_input("RGB: ")
-			req = formatRequest(req)
+    # loop until the user presses ^C
+    try:
+        while(True):
+            req = raw_input("RGB: ")
+            req = formatRequest(req)
 
-			# update all pins with new values if req is invalid it will be an
+            # update all pins with new values if req is invalid it will be an
             # empty list and nothing will happen
-			i = 0
-			for _ in req:
-				GPIO.output(pins[i], req[i])
-				i += 1
+            i = 0
+            for _ in req:
+                GPIO.output(pins[i], req[i])
+                i += 1
 
-	# user pressed ^C so exit
-	except KeyboardInterrupt:
-		GPIO.cleanup()
+    # user pressed ^C so exit
+    except KeyboardInterrupt:
+        GPIO.cleanup()
 
-	return
+    return
 
 
 #   Format Request
@@ -76,23 +76,23 @@ def run():
 #   "010" becomes [True, False, True]
 
 def formatRequest(req):
-	val = []
+    val = []
 
-	# request is a color, convert it to RGB
-	if (req in colors):
-		val = colors[req]
-	# request is RGB, don't modify it
-	elif (len(req) == 3 and re.match('^[0-1]*$', req)):
-		val = req
+    # request is a color, convert it to RGB
+    if (req in colors):
+        val = colors[req]
+    # request is RGB, don't modify it
+    elif (len(req) == 3 and re.match('^[0-1]*$', req)):
+        val = req
 
-	# request is valid, flip its bits (low is active)
-	if (val):
-		val = flipBits(val)
+    # request is valid, flip its bits (low is active)
+    if (val):
+        val = flipBits(val)
 
-	return val
+    return val
 
 
-#	Flip Bits
+#   Flip Bits
 # Accepts a string of 1s and 0s and returns a list of boolean values
 # corresponding to the opposite bit.
 # Example:
@@ -100,12 +100,12 @@ def formatRequest(req):
 #   0 becomes True
 
 def flipBits(str):
-	val = []
+    val = []
 
-	for c in str:
-		val.append(not bool(int(c)))
+    for c in str:
+        val.append(not bool(int(c)))
 
-	return val
+    return val
 
 
 
